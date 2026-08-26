ea6982b0800efb877643ac0c8b79b45df4601146
avrahamNotik <haimnotik@gmail.com>
2026-08-26 14:16:36 +0300

    chore: add backend structure and routes

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,13 @@
 import express from "express";
 import cors from "cors";
 
+import routes from "./routes";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/health", (_req, res) => {
-  res.json({
-    status: "ok",
-    message: "Primary Voting Backend is running",
-  });
-});
+app.use("/api", routes);
 
 export default app;
